6a8abeed071f993cdda3cbb4626bcb609e62dad2
cesaroer <cesarvarcard@gmail.com>
2021-07-28 23:57:11 -0500

    Added product card widget and listView builder

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,15 @@ class MyApp extends StatelessWidget {
         "home": (_) => HomeScreen(),
       },
       theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: Colors.grey[300],
-      ),
+          scaffoldBackgroundColor: Colors.grey[300],
+          appBarTheme: AppBarTheme(
+            elevation: 0,
+            color: Colors.indigo,
+          ),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.indigo,
+            elevation: 0,
+          )),
     );
   }
 }
