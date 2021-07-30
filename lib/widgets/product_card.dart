160aed490ab80d2b50f08506aaa26438eb78f529
cesaroer <cesarvarcard@gmail.com>
2021-07-30 00:03:11 -0500

    Bottom container added

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -11,8 +11,10 @@ class ProductCard extends StatelessWidget {
         height: 400,
         decoration: _cardBorders(),
         child: Stack(
+          alignment: Alignment.bottomLeft,
           children: [
             _BackgroundImage(),
+            _productDetails(),
           ],
         ),
       ),
@@ -32,15 +34,30 @@ class ProductCard extends StatelessWidget {
       );
 }
 
-class _BackgroundImage extends StatelessWidget {
+class _productDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 400,
-      child: FadeInImage(
-        placeholder: AssetImage("assets/jar-loading.gif"),
-        image: NetworkImage("https://via.placeholder.com/400x300"),
+      height: 70,
+      color: Colors.indigo,
+    );
+  }
+}
+
+class _BackgroundImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(35),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage("assets/jar-loading.gif"),
+          image: NetworkImage("https://via.placeholder.com/400x300"),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
